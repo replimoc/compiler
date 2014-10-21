1ebb3bd90405b82f8e76e492d9f034a25a4efdaf
Andreas Eberle <email@andreas-eberle.com>
2014-10-21 15:04:35 +0200

    added dummy test and dummy main, as well as first travis config

---
+++ Compiler/src/compiler/main/CompilerApp.java
@@ -0,0 +1,8 @@
+package compiler.main;
+
+public class CompilerApp {
+
+	public static void main(String args[]) {
+		System.out.println("Hello world!");
+	}
+}
---
+++ Compiler/tests/compiler/main/DummyTest.java
@@ -0,0 +1,14 @@
+package compiler.main;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
