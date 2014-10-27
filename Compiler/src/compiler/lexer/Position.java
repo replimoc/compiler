a520438d31f69b8c6ced14e49091af6d24a43208
Valentin Zickner <zickner@querformatik.de>
2014-10-27 16:54:14 +0100

    Implemented Position class with constructor and getters.

--- Compiler/src/compiler/lexer/Position.java
+++ Compiler/src/compiler/lexer/Position.java
@@ -1,5 +1,19 @@
 package compiler.lexer;
 
 public class Position {
+	private final int line;
+	private final int character;
 
+	public Position(int line, int character) {
+		this.line = line;
+		this.character = character;
+	}
+
+	public int getLine() {
+		return line;
+	}
+
+	public int getCharacter() {
+		return character;
+	}
 }
