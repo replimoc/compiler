bca72f3c4c9b01959125a6246f5ee7bab1663027
Valentin Zickner <zickner@querformatik.de>
2014-10-27 16:16:58 +0100

    Added empty Lexer class with dependencies.

---
+++ Compiler/src/compiler/lexer/Lexer.java
@@ -0,0 +1,16 @@
+package compiler.lexer;
+
+import java.io.BufferedInputStream;
+
+import compiler.StringTable;
+
+public class Lexer {
+	public Lexer(BufferedInputStream bufferedInputStream, StringTable stringTable) throws Exception {
+		throw new Exception("Not implemented yet");
+	}
+
+	public Token getNextToken() {
+		return null;
+
+	}
+}
