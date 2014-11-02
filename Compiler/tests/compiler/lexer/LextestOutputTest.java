8ff9fd891f4473057f85b8f38e837761e002e58e
Andreas Eberle <email@andreas-eberle.com>
2014-11-02 13:12:27 +0100

    Refactored LextestOutputTest

--- Compiler/tests/compiler/lexer/LextestOutputTest.java
+++ Compiler/tests/compiler/lexer/LextestOutputTest.java
@@ -1,22 +1,23 @@
 package compiler.lexer;
 
-import compiler.StringTable;
-import junit.framework.AssertionFailedError;
-import junit.framework.ComparisonFailure;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
-import static java.nio.file.FileVisitResult.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import compiler.StringTable;
 
 /**
  * Test case for correct output of compiler with --lextest option
@@ -25,151 +26,80 @@ import static java.nio.file.FileVisitResult.*;
  */
 public class LextestOutputTest {
 
-    HashSet<String> excludedSourceFiles = new HashSet<String>();
+	public static class LexTester extends SimpleFileVisitor<Path> {
+		private static final String LEXER_EXTENSION = ".lexer";
+		private static final String JAVA_EXTENSION = ".java";
 
-    public static class LexTester extends SimpleFileVisitor<Path> {
-        private static final String lexerExtension = ".lexer";
-        private static final String javaExtension = ".java";
+		private final PathMatcher matcher;
+		private List<Path> failedTestsList = new ArrayList<>();
 
-        private final PathMatcher matcher;
-        private List<Path> failedTestsList = new ArrayList<>();
+		public LexTester() {
+			matcher = FileSystems.getDefault().getPathMatcher("glob:*" + LEXER_EXTENSION);
+		}
 
-        public LexTester() {
-            matcher = FileSystems.getDefault().getPathMatcher("glob:*" + lexerExtension);
-        }
+		@Override
+		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+			Path name = file.getFileName();
+			if (name != null && matcher.matches(name)) {
+				String lexFilename = name.toString();
+				String sourceFilename = lexFilename.replace(LEXER_EXTENSION, JAVA_EXTENSION);
 
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-            Path name = file.getFileName();
-            if (name != null && matcher.matches(name)) {
-                String lexFilename = name.toString();
-                String sourceFilename = lexFilename.replace(lexerExtension, javaExtension);
+				Path sourceFilePath = file.getParent().resolve(sourceFilename);
 
-                Path sourceFilePath = file.getParent().resolve(sourceFilename);
+				if (!Files.exists(sourceFilePath)) {
+					Assert.fail("cannot find program to output " + sourceFilePath);
+				}
 
+				try {
+					testSourceFile(sourceFilePath, file);
+				} catch (Exception e) {
+					System.err.println("Test for file = " + file + " failed");
+					e.printStackTrace();
+					failedTestsList.add(file);
+				} catch (AssertionError e) {
+					System.err.println("Test for file = " + file + " failed");
+					e.printStackTrace();
+					failedTestsList.add(file);
+				}
 
-                if (!Files.exists(sourceFilePath)) {
-                    Assert.fail("cannot find program to output " + sourceFilePath);
-                }
+			}
+			return FileVisitResult.CONTINUE;
+		}
 
-                try {
-                    testSourceFile(sourceFilePath, file);
-                } catch (Exception e) {
-                    System.err.println("Test for file = " + file + " failed");
-                    e.printStackTrace();
-                    failedTestsList.add(file);
-                } catch (AssertionError e) {
-                    System.err.println("Test for file = " + file + " failed");
-                    e.printStackTrace();
-                    failedTestsList.add(file);
-                }
+		public void checkForFailedTests() {
+			if (failedTestsList.size() != 0) {
+				Assert.fail("Tests for " + failedTestsList.size() + " test(s) failed");
+			}
+		}
+	}
 
-            }
-            return CONTINUE;
-        }
+	@Test
+	public void testLexerFiles() throws Exception {
+		Path testDir = Paths.get("testdata");
+		LexTester lexTester = new LexTester();
+		Files.walkFileTree(testDir, lexTester);
+		lexTester.checkForFailedTests();
+	}
 
-        public void checkForFailedTests() {
-            if (failedTestsList.size() != 0) {
-                Assert.fail("Tests for " + failedTestsList.size() + " test(s) failed");
-            }
-        }
-    }
+	private static void testSourceFile(Path sourceFile, Path lexFile) throws Exception {
+		// read expected output
+		BufferedReader expectedOutput = Files.newBufferedReader(lexFile, StandardCharsets.US_ASCII);
+		BufferedInputStream sourceIs = new BufferedInputStream(Files.newInputStream(sourceFile));
 
+		Lexer lexer = new Lexer(sourceIs, new StringTable());
 
-    @Before
-    public void setUp() throws Exception {
-//        excludedSourceFiles.add("empty.lexer");
-    }
+		// compare expected output and actual output line by line
+		String expectedLine;
+		int lineNumber = 0;
 
-    @Test
-    public void testLexerFiles() throws Exception {
-        Path testDir = Paths.get("testdata");
-        LexTester lexTester = new LexTester();
-        Files.walkFileTree(testDir, lexTester);
-        lexTester.checkForFailedTests();
-    }
+		while ((expectedLine = expectedOutput.readLine()) != null) {
+			lineNumber++;
+			Token nextToken = lexer.getNextToken();
+			Assert.assertNotNull("missing output: expected " + expectedLine + " in line " + lineNumber, nextToken);
+			Assert.assertEquals("tokens not equal in line in line " + lineNumber, expectedLine, nextToken.getTokenString());
+		}
 
-    private static void testSourceFile(Path sourceFile, Path lexFile) throws Exception {
-        // read expected output
-        BufferedReader expectedOutput = Files.newBufferedReader(lexFile, StandardCharsets.US_ASCII);
-        BufferedInputStream sourceIs = new BufferedInputStream(Files.newInputStream(sourceFile));
-
-        Lexer lexer = new Lexer(sourceIs, new StringTable());
-
-        // compare expected output and actual output line by line
-        String expectedLine;
-        String actualLine;
-
-        while ((expectedLine = expectedOutput.readLine()) != null) {
-            Token nextToken = lexer.getNextToken();
-            Assert.assertNotNull("missing output: expected " + expectedLine, nextToken);
-            actualLine = nextToken.getTokenString();
-//            System.out.println(actualLine);
-            Assert.assertEquals(expectedLine, actualLine);
-        }
-
-        Token nextToken = lexer.getNextToken();
-        Assert.assertNull("not expected output, expected eof", nextToken);
-    }
-
-    /**
-     * This test takes all expected outputs in $PROJECT_DIR/testdata/lextest-out, finds corresponding sources in
-     * $PROJECT_DIR/testdata/sources,runs compiler with --lextest and sourceFile, and then compares output line by line.
-     * <p/>
-     * This test requires project jar file in target directory
-     * This test can only be run on linux (it calls script ./compiler.sh)
-     * This test must be run from working directory $PROJECT_DIR = compiler/Compiler
-     * (and I don't know how to fix this)
-     * <p/>
-     * ...so it must be called manually
-     * <p/>
-     * to exclude expected results from testing add them to excludedSourceFiles
-     */
-    @Test
-    @Ignore
-    public void testFiles() throws Exception {
-
-        // read files in lextest-out
-        Path lextestDir = Paths.get("testdata/lextest-out");
-        DirectoryStream<Path> stream = Files.newDirectoryStream(lextestDir);
-
-        for (Path lextestFile : stream) {
-            // find corresponding program in testdata/sources
-            String filename = lextestFile.getFileName().toString();
-            if (excludedSourceFiles.contains(filename)) continue;
-
-            String programFilename = "testdata/sources/" + filename.replace(".lexer", ".java");
-
-            if (!Files.exists(Paths.get(programFilename))) {
-                Assert.fail("cannot find program to output " + filename);
-            }
-
-            System.out.println("===== " + programFilename + " =====");
-
-            // run lexer and get output
-            ProcessBuilder pb = new ProcessBuilder("./compiler.sh", "--lextest", programFilename);
-            pb.redirectErrorStream(true);
-            Process compiler = pb.start();
-            BufferedReader compilerOutput = new BufferedReader(new InputStreamReader(compiler.getInputStream()));
-
-            // read expected output
-            BufferedReader expectedOutput = Files.newBufferedReader(lextestFile, StandardCharsets.US_ASCII);
-
-            // compare expected output and actual output line by line
-            String expectedLine;
-            String outputLine;
-
-            while ((expectedLine = expectedOutput.readLine()) != null) {
-                outputLine = compilerOutput.readLine();
-                System.out.println(outputLine);
-                Assert.assertNotNull("missing output: expected " + outputLine, outputLine);
-                Assert.assertEquals(expectedLine, outputLine);
-            }
-
-            outputLine = compilerOutput.readLine();
-            Assert.assertNull("not expected output, expected eof", outputLine);
-
-        }
-
-    }
+		Token nextToken = lexer.getNextToken();
+		Assert.assertNull("not expected output, expected null after file", nextToken);
+	}
 }
