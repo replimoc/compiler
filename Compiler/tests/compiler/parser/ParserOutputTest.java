4889c1c173e686936821b516d57a8ea70996f3da
effenok <polina.goltsman@student.kit.edu>
2014-11-05 15:59:24 +0100

    class to automatically check parser files
    the class searches for files with extension *.parser, then finds appropriate *.java files. *.parser files should contain 1 (for correct grammar) and 0 (for incorrect grammar). The test runs parser on *.java and checks if the parser's result conforms to the result in *.parser file.

---
+++ Compiler/tests/compiler/parser/ParserOutputTest.java
@@ -0,0 +1,107 @@
+package compiler.parser;
+
+import compiler.utils.TestUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Test case for correct output of compiler with only parse phase
+ * <p/>
+ * this test should be started from Compiler directory
+ */
+public class ParserOutputTest {
+
+    public static class LexTester extends SimpleFileVisitor<Path> {
+        private static final String PARSER_EXTENSION = ".parser";
+        private static final String JAVA_EXTENSION = ".java";
+
+        private final PathMatcher matcher;
+        private List<Path> failedTestsList = new ArrayList<>();
+
+        public LexTester() {
+            matcher = FileSystems.getDefault().getPathMatcher("glob:*" + PARSER_EXTENSION);
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+            Path name = file.getFileName();
+            if (name != null && matcher.matches(name)) {
+                String lexFilename = name.toString();
+                String sourceFilename = lexFilename.replace(PARSER_EXTENSION, JAVA_EXTENSION);
+
+                Path sourceFilePath = file.getParent().resolve(sourceFilename);
+
+                if (!Files.exists(sourceFilePath)) {
+                    Assert.fail("cannot find program to output " + sourceFilePath);
+                }
+
+                try {
+                    testSourceFile(sourceFilePath, file);
+                } catch (Exception e) {
+                    System.err.println("Test for file = " + file + " failed");
+                    e.printStackTrace();
+                    failedTestsList.add(file);
+                } catch (AssertionError e) {
+                    System.err.println("Test for file = " + file + " failed");
+                    e.printStackTrace();
+                    failedTestsList.add(file);
+                }
+
+            }
+            return FileVisitResult.CONTINUE;
+        }
+
+        public void checkForFailedTests() {
+            if (failedTestsList.size() != 0) {
+                Assert.fail("Tests for " + failedTestsList.size() + " test(s) failed");
+            }
+        }
+    }
+
+    @Test
+    public void testParserFiles() throws Exception {
+        Path testDir = Paths.get("testdata");
+        LexTester lexTester = new LexTester();
+        Files.walkFileTree(testDir, lexTester);
+        lexTester.checkForFailedTests();
+    }
+
+    private static void testSourceFile(Path sourceFile, Path parserFile) throws Exception {
+        // read expected output
+        List<String> expectedOutput = Files.readAllLines(parserFile);
+        Parser parser = TestUtils.initParser(sourceFile.toString());
+
+        boolean isGrammarValid = false;
+
+        if (expectedOutput.get(0).equals("1")) {
+            isGrammarValid = true;
+        } else if (expectedOutput.get(0).equals("0")) {
+            isGrammarValid = false;
+        } else {
+            Assert.fail("Invalid content in " + parserFile + ", should contain 1 or 0");
+        }
+
+        try {
+            parser.parse();
+            if(!isGrammarValid)
+            {
+                Assert.fail("Parser successful on file " + sourceFile + " which is syntactically incorrect");
+            }
+        } catch (ParserException pe) {
+            if(isGrammarValid)
+            {
+                System.err.println();
+                System.err.println("============================= + " + sourceFile + " + =======================================");
+                pe.printStackTrace();
+                System.err.println("============================= - " + sourceFile + " - =======================================");
+                System.err.println();
+                Assert.fail("Parser failed on file " + sourceFile + " which is syntactically correct");
+            }
+        }
+    }
+}
