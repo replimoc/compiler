765e4148fd7659c6e84b84b8b79bf44ae6df232c
Norman Boewing <abc87545-a@yahoo.de>
2014-11-08 10:59:09 +0100

    Added feedback for errors when possible

--- Compiler/src/compiler/parser/ParserException.java
+++ Compiler/src/compiler/parser/ParserException.java
@@ -1,24 +1,37 @@
 package compiler.parser;
 
 import compiler.lexer.Token;
+import compiler.lexer.TokenType;
 
 public class ParserException extends Exception {
 	private static final long serialVersionUID = -5282537953189117934L;
 
 	private final Token unexpectedToken;
+	private final TokenType expectedToken;
+
+	ParserException(Token t, TokenType expected) {
+		unexpectedToken = t;
+		this.expectedToken = expected;
+	}
 
 	ParserException(Token t) {
 		unexpectedToken = t;
+		expectedToken = null;
 	}
 
-    public Token getUnexpectedToken() {
-        return unexpectedToken;
-    }
+	public Token getUnexpectedToken() {
+		return unexpectedToken;
+	}
 
-    @Override
+	@Override
 	public String toString() {
-		return "Error in line: " + unexpectedToken.getPosition().getLine() + ". Unexpected token '" + unexpectedToken.getTokenString()
-				+ "' at character: " + unexpectedToken.getPosition().getCharacter();
+		if (expectedToken != null) {
+			return "Error in line: " + unexpectedToken.getPosition().getLine() + ". Unexpected token '" + unexpectedToken.getTokenString()
+					+ "' at character: " + unexpectedToken.getPosition().getCharacter() + ". Expected token: '" + expectedToken.getString() + "'";
+		} else {
+			return "Error in line: " + unexpectedToken.getPosition().getLine() + ". Unexpected token '" + unexpectedToken.getTokenString()
+					+ "' at character: " + unexpectedToken.getPosition().getCharacter();
+		}
 	}
 
 	@Override
