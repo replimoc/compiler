700bcabe7bf5c5edb09fd9bbe75a4f3f10b2b47e
Andreas Eberle <email@andreas-eberle.com>
2014-11-19 17:01:35 +0100

    Added fixpoint test for ast parsing with pretty printing (closes #58).

--- Compiler/tests/compiler/parser/AutomatedAstComparisionTest.java
+++ Compiler/tests/compiler/parser/AutomatedAstComparisionTest.java
@@ -2,6 +2,7 @@ package compiler.parser;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -34,11 +35,25 @@ public class AutomatedAstComparisionTest implements TestFileVisitor.FileTester {
 		parserTester.checkForFailedTests();
 	}
 
+	@Test
+	public void testFixpointProperty() throws Exception {
+		Path testDir = Paths.get("testdata");
+		TestFileVisitor parserTester = new TestFileVisitor(PRETTY_AST_EXTENSION, PRETTY_AST_EXTENSION, this);
+		Files.walkFileTree(testDir, parserTester);
+		parserTester.checkForFailedTests();
+	}
+
 	@Override
-	public void testSourceFile(Path sourceFile, Path parserFile) throws Exception {
-		System.out.println("Testing ast generation of " + sourceFile);
+	public void testSourceFile(Path sourceFile, Path expectedFile) throws Exception {
 		// read expected output
-		List<String> expectedOutput = Files.readAllLines(parserFile, StandardCharsets.US_ASCII);
+		List<String> expectedOutput = Files.readAllLines(expectedFile, StandardCharsets.US_ASCII);
+		boolean failingExpected = !expectedOutput.isEmpty() && "error".equals(expectedOutput.get(0));
+
+		if (sourceFile.equals(expectedFile) && failingExpected) { // this is the case if we do the fixpoint test
+			return; // do not test error files
+		}
+
+		System.out.println("Testing ast generation of " + sourceFile);
 		Parser parser = TestUtils.initParser(sourceFile);
 
 		try {
@@ -55,9 +70,10 @@ public class AutomatedAstComparisionTest implements TestFileVisitor.FileTester {
 			assertFalse(s.hasNext());
 			s.close();
 
+			assertFalse(failingExpected);
 		} catch (ParsingFailedException e) {
 			int errors = e.getDetectedErrors();
-			assertEquals(expectedOutput.get(0), "error");
+			assertTrue(failingExpected);
 			if (expectedOutput.size() > 1) {
 				assertEquals(new Integer(expectedOutput.get(1)).intValue(), errors);
 			}
