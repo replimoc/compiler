ab70a484d09a8d57729eb5b0d6a4eaf298a86c32
effenok <polina.goltsman@student.kit.edu>
2014-11-19 17:39:50 +0100

    fixed errors in methods.java

--- Compiler/testdata/semantic5/methods.java
+++ Compiler/testdata/semantic5/methods.java
@@ -2,28 +2,28 @@ class Test {
 
     /* void methods */
 
-    void m10() {};
+    public void m10() {;}
 
-    void m11() { return };
+    public void m11() { return; }
 
     /* return new primitive types and variables */
 
-    int m20() {return 1;}
-    int m21() {int x = 5 ; return x;}
-    boolean m22() {boolean x = true ; return x;}
-    boolean m23() {int x = 5 ; return x == 5;}
+    public int m20() {return 1;}
+    public int m21() {int x = 5 ; return x;}
+    public boolean m22() {boolean x = true ; return x;}
+    public boolean m23() {int x = 5 ; return x == 5;}
 
     /* methods with return types must have returns in every path */
 
-    int m31() {
+    public int m31() {
         if (true) {
             return 1;
         } else {
             return 2;
         }
-    };
+    }
 
-    int m32() {
+    public int m32() {
         int x;
         if (true) {
             x = 1;
@@ -31,9 +31,9 @@ class Test {
             x = 2;
         }
         return x;
-    };
+    }
 
-    int m33() {
+    public int m33() {
         int x;
         if (true) {
             return 5;
@@ -41,14 +41,14 @@ class Test {
             x = 2;
         }
         return x;
-    };
+    }
 
-    int m34() {
+    public int m34() {
         while(true);
         return 1;
     }
 
-    int m35() {
+    public int m35() {
         while(true) {
             return 1;
         }
@@ -56,32 +56,32 @@ class Test {
 
     /* return objects */
 
-    Test m40 () { return null; }
-    Test m41 () { return this; }
-    Test m42 () { return new Test(); }
-    Test m43 () { Test x = new Test(); return x; }
+    public Test m40 () { return null; }
+    public Test m41 () { return this; }
+    public Test m42 () { return new Test(); }
+    public Test m43 () { Test x = new Test(); return x; }
 
     /* arrays */
 
-    boolean[] m50 () {return new boolean[1];}
-    int[] m51 () {return new int[1];}
-    Test[] m52 () {return new Test[1];}
+    public boolean[] m50 () {return new boolean[1];}
+    public int[] m51 () {return new int[1];}
+    public Test[] m52 () {return new Test[1];}
 
     /* methods with parameters */
-    void f1(int x) {};
-    void f2(int x, int y) {};
-    void f3(int x, boolean b) {};
-    void f4(Test x) {};
-    void f5(Test[] x) {};
-    void f6(int[] x) {};
-    void f7(boolean[] x) {};
+    public void f1(int x) {}
+    public void f2(int x, int y) {}
+    public void f3(int x, boolean b) {}
+    public void f4(Test x) {}
+    public void f5(Test[] x) {}
+    public void f6(int[] x) {}
+    public void f7(boolean[] x) {}
 
     public static void main(String[] args) {
 
         /* test correct parameters */
 
         int x; boolean b; Test test;
-        int[] xx; boolean bb[]; Test[] tt;
+        int[] xx; boolean[] bb; Test[] tt;
 
         f1(0); f1(x); f1(x+1); f1(x-1); f1(xx[1]);
         f2(1,2); f2(x,x); f2(x*x, x/x);
@@ -105,11 +105,11 @@ class Test {
         bb[0] = m22();
         tt[0] = m43();
 
-        boolean bbb[] = m50();
+        boolean[] bbb = m50();
         bb = m50();
-        int yyy = m51();
+        int[] yyy = m51();
         xx = m51();
-        Test ttt[] = m52();
+        Test[] ttt = m52();
         tt = m52();
     }
 }
