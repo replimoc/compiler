f7c9efdd66e775afe7726ef1c1cfff0765bc6aa9
Andreas Eberle <email@andreas-eberle.com>
2014-11-23 14:08:17 +0100

    Removed obsolete code

--- Compiler/src/compiler/semantic/symbolTable/Scope.java
+++ Compiler/src/compiler/semantic/symbolTable/Scope.java
@@ -18,32 +18,4 @@ public class Scope {
 		return oldChangeStackSize;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + oldChangeStackSize;
-		result = prime * result + ((parentScope == null) ? 0 : parentScope.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Scope other = (Scope) obj;
-		if (oldChangeStackSize != other.oldChangeStackSize)
-			return false;
-		if (parentScope == null) {
-			if (other.parentScope != null)
-				return false;
-		} else if (!parentScope.equals(other.parentScope))
-			return false;
-		return true;
-	}
-
 }
