fad49ee6af6f0872a9bbf35a8423267f1b6dd751
effenok <polina.goltsman@student.kit.edu>
2014-11-23 19:07:21 +0100

    another test file

--- Compiler/testdata/pw-tests/correct_2.java
+++ Compiler/testdata/pw-tests/correct_2.java
@@ -1,14 +1,15 @@
 class xl8JuK4Y3rbR3nFI0rPkaQLCUcYvyvdLNnoip09ebjGtbnV5vZwuYlSKCD0iVEr1SnTye1nD6zfrBZyvSgV78CaZc6MXlagHPGqunhERuKwpJ5MjvDpaY5BfYY6PCD8ReCgSfUt82r2w06147czVlZWzuVWPftW9Lb2frNTaecOYTJbryYrxfpPJZabT8QC9CgPQB2bv {
-    public void[][][][][][][] foo(boolean bar) {
-        boolean[] baz = 42 = (null = 42);
-        return;
+    public void[][][][][][][] foo(boolean bar) { /* semantic error void[] */
+        boolean[] baz = 42 = (null = 42); /* semantic error x2 */
+        return; /* semantic error  */
     }
     public XxsV9 RDDcwt9;
 }
 
 class _ {
     public _[][] _;
-    public __0_ _0__ (_0I_ _oO0) {
+
+    public __0_ _0__ (_0I_ _oO0) { /* semantic error  _OI_ is not defined */
         {
             {
                 {
@@ -21,9 +22,9 @@ class _ {
                                             {
                                                 {
                                                     {
-                                                        _._;
+                                                        _._; /* semantic error first _ is a variable of array of []  */
                                                     }
-                                                    _._((((this = _ * this))), new _[_][]);
+                                                    _._((((this = _ * this))), new _[_][]); /* maybe an error, but i can't read this*/
                                                 }
                                             }
                                         }
@@ -51,10 +52,12 @@ class _ {
 class a {
     public int foo() {
         {
-            return this * 1337;
+            return this * 1337; /* semantic error  */
         }
         {
-            return;
+            return; /* semantic error  */
         }
     }
 }
+
+/* semantic error - no main */
