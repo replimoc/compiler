26f83ae3775d6185c8d1d261601a24ab9fd252a0
Andreas Eberle <email@andreas-eberle.com>
2014-11-23 21:07:00 +0100

    Added some unit tests and improved coverage

--- Compiler/src/compiler/ast/type/ClassType.java
+++ Compiler/src/compiler/ast/type/ClassType.java
@@ -22,4 +22,29 @@ public class ClassType extends Type {
 	public void accept(AstVisitor visitor) {
 		visitor.visit(this);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((identifier == null) ? 0 : identifier.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClassType other = (ClassType) obj;
+		if (identifier == null) {
+			if (other.identifier != null)
+				return false;
+		} else if (!identifier.equals(other.identifier))
+			return false;
+		return true;
+	}
 }
