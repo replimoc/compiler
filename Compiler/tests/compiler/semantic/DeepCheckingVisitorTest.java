05771330beb1c020565bf8c8353d8aa00e5e382b
Aleksej Frank <aleksejfrank@googlemail.com>
2014-11-23 21:28:46 +0100

    fixed deep checking visitor test

--- Compiler/tests/compiler/semantic/DeepCheckingVisitorTest.java
+++ Compiler/tests/compiler/semantic/DeepCheckingVisitorTest.java
@@ -183,7 +183,7 @@ public class DeepCheckingVisitorTest {
 		parser = TestUtils
 				.initParser("class Main{public static void main(String[] vargs){vargs[5];}}");
 		errors = SemanticChecker.checkSemantic(parser.parse());
-		assertEquals(1, errors.size());
+		assertEquals(2, errors.size());
 		
 		parser = TestUtils
 				.initParser("class Main{public static void main(String[] vargs){int a = a;}}");
@@ -193,7 +193,7 @@ public class DeepCheckingVisitorTest {
 		parser = TestUtils
 				.initParser("class Main{public static void asdf(String[] vargs){return 0;}}");
 		errors = SemanticChecker.checkSemantic(parser.parse());
-		assertEquals(1, errors.size());
+		assertEquals(2, errors.size());
 	}
 
 	@Test
