d6a95964aabf79f89562db0fd2a8b91b0fc15906
Andreas Eberle <email@andreas-eberle.com>
2014-11-27 00:03:00 +0100

    Moved FirmMethodsTest to firm package.

--- Compiler/tests/compiler/firm/FirmMethodsTest.java
+++ Compiler/tests/compiler/firm/FirmMethodsTest.java
@@ -1,16 +1,11 @@
-package compiler.semantic;
+package compiler.firm;
 
-import org.apache.commons.math3.analysis.function.Constant;
 import org.junit.Test;
 
-import compiler.firm.FirmUtils;
-
 import firm.ClassType;
-import firm.CompoundType;
 import firm.Construction;
 import firm.Dump;
 import firm.Entity;
-import firm.Firm;
 import firm.Graph;
 import firm.MethodType;
 import firm.Mode;
@@ -24,38 +19,34 @@ public class FirmMethodsTest {
 	@Test
 	public void testjFirmInit() {
 		FirmUtils.initFirm();
-		
-		System.out.printf("Initialized Firm Version: %1s.%2s\n",
-				Firm.getMajorVersion(), Firm.getMinorVersion());
 
 		PrimitiveType intType = new PrimitiveType(Mode.getIs());
-		PrimitiveType floatType = new PrimitiveType(Mode.getF());
-		
+
 		ClassType globalType = Program.getGlobalType();
-		
+
 		// create method named foo(int, int) : int
 		MethodType methodType = new MethodType(new Type[] { intType, intType },
 				new Type[] { intType });
 		Entity methodEntity = new Entity(globalType, "foo", methodType);
 		methodEntity.setLdIdent("foo");
-		
+
 		int n_vars = 2 + 1; // two parameters + local var
 		Graph graph = new Graph(methodEntity, n_vars);
 		Construction construction = new Construction(graph);
-		
+
 		// create a const node
 		Node returnConst = construction.newConst(42, Mode.getIs());
 		// set var 2 to const node
 		construction.setVariable(2, returnConst);
-		
+
 		Node nreturn = construction.newReturn(construction.getCurrentMem(), new Node[] { returnConst });
 		graph.getEndBlock().addPred(nreturn);
-		
+
 		construction.setUnreachable();
-		
+
 		construction.finish();
 		Dump.dumpGraph(graph, "-after-construction");
-		
+
 		FirmUtils.finishFirm();
 	}
 }
