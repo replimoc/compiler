3ba7ae7bd8692ccbd46657e4ceb186dc934a4a92
Andreas Eberle <email@andreas-eberle.com>
2014-11-30 14:02:13 +0100

    Changed FirmUtils to respect already set jna.library.path

--- Compiler/src/compiler/firm/FirmUtils.java
+++ Compiler/src/compiler/firm/FirmUtils.java
@@ -4,11 +4,16 @@ import firm.Firm;
 
 public final class FirmUtils {
 
+	private static final String JNA_LIBRARY_PATH = "jna.library.path";
+	private static final String LIB_FIRM_FOLDER = "lib/firm/";
+
 	private FirmUtils() { // no objects of this class shall be created
 	}
 
 	public static void initFirm() {
-		System.setProperty("jna.library.path", "lib/firm/");
+		if (System.getProperty(JNA_LIBRARY_PATH) == null) {
+			System.setProperty(JNA_LIBRARY_PATH, LIB_FIRM_FOLDER);
+		}
 		Firm.init();
 
 		System.out.printf("Initialized libFirm Version: %1s.%s\n", Firm.getMajorVersion(), Firm.getMinorVersion());
