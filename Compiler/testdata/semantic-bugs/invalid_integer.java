0af144b75a683601ac650b59ba01d76929492628
Valentin Zickner <zickner@querformatik.de>
2014-11-30 14:27:42 +0100

    [BUGFIX] Integer should be in the expected interval (fixes #122).

--- Compiler/testdata/semantic-bugs/invalid_integer.java
+++ Compiler/testdata/semantic-bugs/invalid_integer.java
@@ -1,6 +1,6 @@
 class Test {
 	public static void main(String[] args) {
 		int test = 2147483649; /* semantic error, to large */
-		int test = 2147483640; /* no semantic error */
+		int test2 = 2147483640; /* no semantic error */
 	}
 }
