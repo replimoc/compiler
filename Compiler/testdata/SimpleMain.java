2d7017de973afdc209e7800bb5b1a814449895c8
Andreas Eberle <email@andreas-eberle.com>
2014-12-01 19:56:21 +0100

    Adding SimpleMain.java testfile with two System.out.printlns causing memory access violation FirmUtils.createAssembler() (refs #134)

---
+++ Compiler/testdata/SimpleMain.java
@@ -0,0 +1,12 @@
+class EmptyMain {
+
+    /***
+     * super empty method !
+     * @param args
+     */
+	public static void main(String[] args) {
+		System.out.println(4);
+		System.out.println(2);
+	}
+
+}
