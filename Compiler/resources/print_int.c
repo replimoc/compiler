e2e833518b2443f4d2e66d8a7949b943398d7399
Valentin Zickner <zickner@querformatik.de>
2014-12-01 16:34:02 +0100

    Implemented option for generating a binary executable (refs #128, #130, #131, #132).

---
+++ Compiler/resources/print_int.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void print_int(int i) {
+	printf("%d\n", i);
+}
--- Compiler/src/compiler/firm/FirmUtils.java
+++ Compiler/src/compiler/firm/FirmUtils.java
@@ -1,5 +1,6 @@
 package compiler.firm;
 
+import java.io.File;
 import java.io.IOException;
 
 import firm.Backend;
@@ -9,6 +10,7 @@ public final class FirmUtils {
 
 	private static final String JNA_LIBRARY_PATH = "jna.library.path";
 	private static final String LIB_FIRM_FOLDER = "lib/firm/";
+	private static final String ISA_AMD64 = "isa=amd64";
 
 	private FirmUtils() { // no objects of this class shall be created
 	}
@@ -23,7 +25,49 @@ public final class FirmUtils {
 	}
 
 	public static void createAssembler(String outputFileName) throws IOException {
+		Backend.option(ISA_AMD64);
 		Backend.createAssembler(outputFileName, "<builtin>");
+	}
+
+	/**
+	 * Expect escaped outputFileName.
+	 * 
+	 * @param outputFileName
+	 *            File for the binary executable.
+	 * @throws IOException
+	 */
+	public static void createBinary(String outputFileName) throws IOException {
+		File assembler = File.createTempFile("assembler", ".s");
+		File build = File.createTempFile("build", ".o");
+		String assemblerFile = assembler.getAbsolutePath();
+		String buildFile = build.getAbsolutePath();
+
+		System.out.println(assemblerFile);
+		createAssembler(assemblerFile);
+		String gcc = "gcc";
+		if (isWindows()) {
+			gcc += ".exe";
+		}
+
+		systemExec(new String[] { gcc, "-c", assemblerFile, "-o", buildFile });
+		systemExec(new String[] { gcc, "-c", "resources/print_int.c", "-o", "resources/print_int.o" });
+		systemExec(new String[] { gcc, "-o", outputFileName, buildFile, "resources/print_int.o" });
+
+		assembler.delete();
+		build.delete();
+	}
+
+	public static boolean isWindows() {
+		return System.getProperty("os.name").startsWith("Windows");
+	}
+
+	public static void systemExec(String[] strings) throws IOException {
+		Process p = Runtime.getRuntime().exec(strings);
+		try {
+			p.waitFor();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void finishFirm() {
