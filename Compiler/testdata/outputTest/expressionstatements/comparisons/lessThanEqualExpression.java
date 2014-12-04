48042f5081b26d4a325f2c074dabb962ef7ad8d2
Aleksej Frank <aleksejfrank@googlemail.com>
2014-12-04 16:54:05 +0100

    added tests for comparison expressions

---
+++ Compiler/testdata/outputTest/expressionstatements/comparisons/lessThanEqualExpression.java
@@ -0,0 +1,8 @@
+class Test {
+	public static void main(String[] args) {
+		boolean a = (3 <= 4);
+    if (a) {
+      int i = 1 / 0;
+    }
+	}
+}
---
+++ Compiler/testdata/outputTest/expressionstatements/comparisons/lessThanExpression.java
@@ -0,0 +1,9 @@
+class Test {
+	public static void main(String[] args) {
+		boolean a = (3 < 4);
+    if (a) {
+    } else {
+      int i = 1 / 0;
+    }
+	}
+}
