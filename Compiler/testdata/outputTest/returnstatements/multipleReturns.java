3e0b1b719c9b969c5736ab214d5a13196ca8e9d2
norman465 <abc87545-a@yahoo.de>
2014-12-04 17:53:07 +0100

    Added test for multiple returns

---
+++ Compiler/testdata/outputTest/returnstatements/multipleReturns.java
@@ -0,0 +1,18 @@
+class Test {
+	
+	public static void main(String[] args) {
+		Test t = new Test();
+		System.out.println(t.method(1));
+		System.out.println(t.method(2));
+		System.out.println(t.method(3));
+	}
+	
+	public int method(int x) {
+		if(x == 1) {
+			return x;
+		} else {
+			return x+x;
+		}
+		return x+x+x;
+	}
+}
