e8fa812e260f9dc9800ccfe6603056de6fa80481
norman465 <abc87545-a@yahoo.de>
2014-12-04 18:53:42 +0100

    Added tests

---
+++ Compiler/testdata/outputTest/ifstatements/noElse.java
@@ -0,0 +1,8 @@
+class Test {
+	public static void main(String[] args) {
+		if(true) {
+			System.out.println(1);
+		}
+		System.out.println(2);
+	}
+}
---
+++ Compiler/testdata/outputTest/returnstatements/emptyReturn.java
@@ -0,0 +1,11 @@
+class Test {
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.method();
+		System.out.println(1);
+	}
+	
+	public void method() {
+		return;
+	}
+}
