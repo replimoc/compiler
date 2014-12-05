1a8b7aff084b8f79399172cf5006c79a51f9a974
Aleksej Frank <aleksejfrank@googlemail.com>
2014-12-05 16:12:40 +0100

    added text for bug ref #154

---
+++ Compiler/testdata/outputTest/bugs/whileBug.java
@@ -0,0 +1,11 @@
+class App {
+    public App method(int size) {
+        App firstNode = new App();
+        int i = 1;
+        while (i < size) {
+        }       
+        return firstNode;
+    }
+    public static void main(String[] args) {
+    }
+}
