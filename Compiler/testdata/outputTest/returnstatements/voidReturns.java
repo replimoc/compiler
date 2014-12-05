5e7627e41722f51993bf30bc5e8dbbfb15c1f178
norman465 <abc87545-a@yahoo.de>
2014-12-05 10:26:16 +0100

    Added test for multiple returns

---
+++ Compiler/testdata/outputTest/returnstatements/voidReturns.java
@@ -0,0 +1,12 @@
+class Test {
+	public static void main(String[] args) {
+		if(false) {
+			System.out.println(2);
+			return;
+		}
+		System.out.println(1);
+		return;
+		System.out.println(1);
+		return;
+	}
+}
