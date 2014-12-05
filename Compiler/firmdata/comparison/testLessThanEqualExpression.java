684f88aabb2d7dc50adba110d2247ff0fceba16d
Valentin Zickner <zickner@querformatik.de>
2014-12-05 01:43:25 +0100

    Rewritten tests for binary expressions and comparison.

--- Compiler/firmdata/comparison/testLessThanEqualExpression.java
+++
@@ -1,5 +0,0 @@
-class Test {
-	public static void main(String[] args) {
-		boolean a = (3 <= 4);
-	}
-}
--- Compiler/firmdata/expression/testAdditionExpression.java
+++
@@ -1,5 +0,0 @@
-class Test {
-	public static void main(String[] args) {
-		int a = 3 + 4;
-	}
-}
