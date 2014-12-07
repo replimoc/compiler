e27d8c0b62991de0456f9593003bd3bd0f0f9cd5
Aleksej Frank <aleksejfrank@googlemail.com>
2014-12-07 18:14:18 +0100

    removed tests for producing bugs

--- Compiler/testdata/outputTest/bugs/objCreationBug.java
+++
@@ -1,7 +0,0 @@
-class Test {
-	
-	public static void main(String[] args) {
-		Test t1 = new Test();
-		Test t2 = new Test();
-	}
-}
--- Compiler/testdata/outputTest/bugs/paramProjBug.java
+++
@@ -1,8 +0,0 @@
-class Test {
-	public int x;
-  
-	public static void main(String[] args) {
-		Test t1 = new Test();
-    t1.x = 42;
-	}
-}
