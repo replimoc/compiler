037def2be3483cfb7a546414937fe6a21323e720
Valentin Zickner <zickner@querformatik.de>
2014-12-07 08:51:46 +0100

    Added more operation vs sign tests (refs #158).

---
+++ Compiler/testdata/negative-integer/OperationVsSign.java
@@ -0,0 +1,11 @@
+class OperationVsSign {
+	public static void main(String[] args) {
+		int i = 0;
+		int j = i +1; /* operation */
+		int k = i -1; /* operation */
+		int l = i + -1; /* sign */
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(l);
+	}
+}
