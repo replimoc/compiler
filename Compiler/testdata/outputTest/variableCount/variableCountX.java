a6e64fde366c7a0a27cdeb96690f40e87d3736c6
Valentin Zickner <zickner@querformatik.de>
2014-12-07 15:32:39 +0100

    Added more variableCount test files (refs #160).

---
+++ Compiler/testdata/outputTest/variableCount/variableCountX.java
@@ -0,0 +1,144 @@
+class VariableCount {
+	public static void main(String[] args){
+		int ba=0;
+		int bb=0;
+		int bc=0;
+		int bd=0;
+		int be=0;
+		int bf=0;
+		int bg=0;
+		int bh=0;
+		int bi=0;
+		int bj=0;
+		int bk=0;
+		int bl=0;
+		int bm=0;
+		int bn=0;
+		int bo=0;
+		int bp=0;
+		int bq=0;
+		int br=0;
+		int bs=0;
+		int bt=0;
+		int bu=0;
+		int bv=0;
+		int bw=0;
+		int bx=0;
+		int by=0;
+		int bz=0;
+
+		int ca=0;
+		int cb=0;
+		int cc=0;
+		int cd=0;
+		int ce=0;
+		int cf=0;
+		int cg=0;
+		int ch=0;
+		int ci=0;
+		int cj=0;
+		int ck=0;
+		int cl=0;
+		int cm=0;
+		int cn=0;
+		int co=0;
+		int cp=0;
+		int cq=0;
+		int cr=0;
+		int cs=0;
+		int ct=0;
+		int cu=0;
+		int cv=0;
+		int cw=0;
+		int cx=0;
+		int cy=0;
+		int cz=0;
+
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+		{int a=0;int b=0;int c=0;int d=0;int e=0;int f=0;int g=0;int h=0;int i=0;int j=0;int k=0;int l=0;int m=0;int n=0;int o=0;int p=0;int q=0;int r=0;int s=0;int t=0;int u=0;int v=0;int w=0;int x=0;int y=0;int z=0;}
+
+		int aa=0;
+		int ab=0;
+		int ac=0;
+		int ad=0;
+		int ae=0;
+		int af=0;
+		int ag=0;
+		int ah=0;
+		int ai=0;
+		int aj=0;
+		int ak=0;
+		int al=0;
+		int am=0;
+		int an=0;
+		int ao=0;
+		int ap=0;
+		int aq=0;
+		int ar=0;
+		int as=0;
+		int at=0;
+		int au=0;
+		int av=0;
+		int aw=0;
+		int ax=0;
+		int ay=0;
+		int az=0;
+		int a=0;
+		int b=0;
+		int c=0;
+		int d=0;
+		int e=0;
+		int f=0;
+		int g=0;
+		int h=0;
+		int i=0;
+		int j=0;
+		int k=0;
+		int l=0;
+		int m=0;
+		int n=0;
+		int o=0;
+		int p=0;
+		int q=0;
+		int r=0;
+		int s=0;
+		int t=0;
+		int u=0;
+		int v=0;
+		int w=0;
+		int x=0;
+		int y=0;
+		int z=0;
+	}
+}
