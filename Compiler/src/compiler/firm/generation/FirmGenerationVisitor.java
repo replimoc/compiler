31a64103e53ff54f5d4d1837b48703b205e43a01
Valentin Zickner <zickner@querformatik.de>
2014-12-12 14:37:44 +0100

    Moved FirmHierarchy.initialize() to FirmGenerationVisitor.

--- Compiler/src/compiler/firm/generation/FirmGenerationVisitor.java
+++ Compiler/src/compiler/firm/generation/FirmGenerationVisitor.java
@@ -1,8 +1,11 @@
 package compiler.firm.generation;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import compiler.Symbol;
 import compiler.ast.Block;
 import compiler.ast.ClassDeclaration;
 import compiler.ast.ClassMember;
@@ -50,6 +53,7 @@ import compiler.ast.type.ClassType;
 import compiler.ast.type.Type;
 import compiler.ast.visitor.AstVisitor;
 import compiler.firm.FirmUtils;
+import compiler.semantic.ClassScope;
 
 import firm.Construction;
 import firm.Entity;
@@ -81,11 +85,65 @@ public class FirmGenerationVisitor implements AstVisitor {
 	private firm.nodes.Block falseDestination;
 
 	public FirmGenerationVisitor() {
+		Mode.setDefaultModeP(FirmUtils.getModeReference());
+
 		// void* calloc_proxy (size_t num, size_t size);
 		MethodType callocType = new MethodType(
 				new firm.Type[] { new PrimitiveType(FirmUtils.getModeInteger()), new PrimitiveType(FirmUtils.getModeInteger()) },
 				new firm.Type[] { new PrimitiveType(FirmUtils.getModeReference()) });
 		calloc = new Entity(firm.Program.getGlobalType(), "calloc_proxy", callocType);
+	}
+
+	public void initialize(HashMap<Symbol, ClassScope> classScopes) {
+		// first iterate over all classes -- so that forward references to classes
+		// in method parameters and return types work
+		for (Entry<Symbol, ClassScope> currentEntry : classScopes.entrySet()) {
+			ClassDeclaration classDeclaration = currentEntry.getValue().getClassDeclaration();
+
+			// Create class nodes
+			classDeclaration.getType().getFirmClassType();
+			classDeclaration.getType().getFirmType();
+		}
+
+		// iterate over all fields and methods and create firm entities
+		for (Entry<Symbol, ClassScope> currentEntry : classScopes.entrySet()) {
+			ClassScope scope = currentEntry.getValue();
+			ClassDeclaration classDeclaration = scope.getClassDeclaration();
+			firm.ClassType firmClassType = classDeclaration.getType().getFirmClassType();
+
+			// Create field declarations
+			for (Declaration currentField : scope.getFieldDefinitions()) {
+				new Entity(firmClassType, currentField.getAssemblerName(), currentField.getType().getFirmType());
+			}
+			for (MethodDeclaration currentMethod : scope.getMethodDefinitions()) {
+				List<ParameterDefinition> parameterDefinitions = currentMethod.getValidParameters();
+
+				// types of parameters
+				// first parameter is "this" with type referenceToClass
+				firm.Type[] parameterTypes = new firm.Type[parameterDefinitions.size() + 1];
+				parameterTypes[0] = classDeclaration.getType().getFirmType();
+				for (int paramIdx = 0; paramIdx < parameterDefinitions.size(); paramIdx++) {
+					parameterTypes[paramIdx + 1] = parameterDefinitions.get(paramIdx).getType().getFirmType();
+				}
+
+				// return type
+				firm.Type[] returnType = {};
+				if (!currentMethod.getType().is(BasicType.VOID)) {
+					returnType = new firm.Type[1];
+					returnType[0] = currentMethod.getType().getFirmType();
+				}
+
+				// create methodType and methodEntity
+				MethodType methodType = new MethodType(parameterTypes, returnType);
+
+				// create new entity and attach to currentClass
+				new Entity(firmClassType, currentMethod.getAssemblerName(), methodType);
+			}
+
+			firm.ClassType classType = classDeclaration.getType().getFirmClassType();
+			classType.layoutFields();
+			classType.finishLayout();
+		}
 	}
 
 	private Node getThisPointer() {
