d96439a636e08d33fbe8cdd6c28f8469657583e4
Valentin Zickner <zickner@querformatik.de>
2014-12-16 12:56:10 +0100

    Adapted basic blocks to work with windows (refs #196).

--- Compiler/src/compiler/firm/backend/operations/FunctionSpecificationOperation.java
+++ Compiler/src/compiler/firm/backend/operations/FunctionSpecificationOperation.java
@@ -1,5 +1,7 @@
 package compiler.firm.backend.operations;
 
+import compiler.utils.Utils;
+
 public class FunctionSpecificationOperation extends AssemblerOperation {
 
 	private String name;
@@ -10,7 +12,10 @@ public class FunctionSpecificationOperation extends AssemblerOperation {
 
 	@Override
 	public String toString() {
-		return String.format("\t.globl %s\n\t.type\t%s, @function\n", name, name);
+		if (Utils.isWindows()) {
+			return String.format("\t.globl %s\n\t.def\t%s;  .scl    2;      .type   32;     .endef\n", name, name);
+		} else {
+			return String.format("\t.globl %s\n\t.type\t%s, @function\n", name, name);
+		}
 	}
-
 }
