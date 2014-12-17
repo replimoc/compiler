577921310177e7f657628a14f0da13114d49ab5d
Aleksej Frank <aleksejfrank@googlemail.com>
2014-12-17 19:14:07 +0100

    more comments on argument passing in print_int

--- Compiler/src/compiler/firm/backend/X8664AssemblerGenerationVisitor.java
+++ Compiler/src/compiler/firm/backend/X8664AssemblerGenerationVisitor.java
@@ -237,6 +237,8 @@ public class X8664AssemblerGenerationVisitor implements NodeVisitor {
 				 * Does it make sense handling Proj nodes? I think we should ignore them and start from index 3
 				 * 
 				 * If we start iterating from index 3, than the first parameter should be passed in ESI register. (print_int expects it at least)
+				 * 
+				 * Currently the order EDI, ESI, ... seems to be wrong at least for print_int.
 				 */
 				for (int i = 2; i < predCount && (i - 2) < callingRegisters.length; i++) {
 					// Copy parameters in registers for System-V calling convention
