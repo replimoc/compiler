267abd6b9da07391860aa05e7433b493e171fddc
Valentin Zickner <zickner@querformatik.de>
2014-12-17 17:23:46 +0100

    Added missing AddqOperation.

---
+++ Compiler/src/compiler/firm/backend/operations/bit64/AddqOperation.java
@@ -0,0 +1,17 @@
+package compiler.firm.backend.operations.bit64;
+
+import compiler.firm.backend.operations.templates.StorageRegisterOperation;
+import compiler.firm.backend.storage.Register;
+import compiler.firm.backend.storage.Storage;
+
+public class AddqOperation extends StorageRegisterOperation {
+
+	public AddqOperation(Storage storage, Register destination) {
+		super(storage, destination);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("\taddq %s, %s\n", getStorage(), getDestination());
+	}
+}
