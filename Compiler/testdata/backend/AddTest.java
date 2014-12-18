c4d5e1bce8f4f8cc5c754096990d215ea853ad1d
Andreas Eberle <email@andreas-eberle.com>
2014-12-18 17:01:03 +0100

    Formated test backend test files

--- Compiler/testdata/backend/AddTest.java
+++ Compiler/testdata/backend/AddTest.java
@@ -1,14 +1,14 @@
 class AddTest {
 	public static void main(String[] args) {
-    int a = 42;
-    int b = 58;
-    System.out.println(a + b);
-    System.out.println(50 + 50);
-    
-    int c = 2147483647;
-    int d = -2147483647;
-    System.out.println(c);
-    System.out.println(c+d);
-    System.out.println(c+1);
+		int a = 42;
+		int b = 58;
+		System.out.println(a + b);
+		System.out.println(50 + 50);
+
+		int c = 2147483647;
+		int d = -2147483647;
+		System.out.println(c);
+		System.out.println(c + d);
+		System.out.println(c + 1);
 	}
 }
--- Compiler/testdata/backend/MulTest.java
+++ Compiler/testdata/backend/MulTest.java
@@ -1,8 +1,8 @@
 class Test {
 	public static void main(String[] args) {
-      System.out.println(50 * 50);
-	    int a = 42;
-	    int b = 2;
-	    System.out.println(a * b);
+		System.out.println(50 * 50);
+		int a = 42;
+		int b = 2;
+		System.out.println(a * b);
 	}
 }
