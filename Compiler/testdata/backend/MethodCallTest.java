ffa08e89ffc0e41c78dd781a5eed7214f7bf9cbd
Valentin Zickner <zickner@querformatik.de>
2014-12-18 02:09:07 +0100

    Added test file for possible method calls.

---
+++ Compiler/testdata/backend/MethodCallTest.java
@@ -0,0 +1,24 @@
+class Test {
+	public static void main(String[] args) {
+		Test t = new Test();
+		int b;
+		b = t.a(2336);
+		System.out.println(b);
+	}
+
+	public int a(int a) {
+		int c = 10;
+		Test d = b();
+		d.d();
+		return c;
+	}
+
+	public Test b() {
+		System.out.println(1337);
+		return new Test();
+	}
+
+	public void d() {
+		System.out.println(42);
+	}
+}
