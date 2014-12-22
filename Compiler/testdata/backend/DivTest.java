1305ae1ef0f1fc36fe8554782ab31c468acd6027
Andreas Eberle <email@andreas-eberle.com>
2014-12-23 00:52:30 +0100

    Added .result files for backend test files.

--- Compiler/testdata/backend/DivTest.java
+++ Compiler/testdata/backend/DivTest.java
@@ -1,10 +1,10 @@
 class Test {
 	public static void main(String[] args) {
-    System.out.println(50 / 2);
-    System.out.println(50 / 1);
-    System.out.println(100 / 10);
-    int a = 42;
-    int b = 2;
-    System.out.println(a / b);
+		System.out.println(50 / 2);
+		System.out.println(50 / 1);
+		System.out.println(100 / 10);
+		int a = 42;
+		int b = 2;
+		System.out.println(a / b);
 	}
 }
