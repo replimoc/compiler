4f946820ae39bf660e3a849a248c0c718d14ce20
Andreas Eberle <email@andreas-eberle.com>
2015-01-09 00:14:22 +0100

    Implemented correct block ordering (own implementation instead of firm's Graph.walkBlocksPostOrder()

--- Compiler/src/compiler/firm/backend/AssemblerGenerator.java
+++ Compiler/src/compiler/firm/backend/AssemblerGenerator.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 import compiler.firm.backend.calling.CallingConvention;
@@ -19,7 +20,9 @@ import firm.BackEdges;
 import firm.BlockWalker;
 import firm.Graph;
 import firm.Program;
+import firm.bindings.binding_irgraph;
 import firm.nodes.Block;
+import firm.nodes.Node;
 
 public final class AssemblerGenerator {
 
@@ -48,7 +51,7 @@ public final class AssemblerGenerator {
 			BackEdges.enable(graph);
 
 			final HashMap<Block, BlockNodes> nodesPerBlockMap = collectorVisitor.getNodesPerBlockMap();
-			graph.walkBlocksPostorder(new BlockWalker() {
+			walkBlocksPostOrder(graph, new BlockWalker() {
 				@Override
 				public void visitBlock(Block block) {
 					nodesPerBlockMap.get(block).visitNodes(visitor, nodesPerBlockMap);
@@ -66,6 +69,55 @@ public final class AssemblerGenerator {
 		}
 
 		generateAssemblerFile(outputFile, assembler);
+	}
+
+	private static void walkBlocksPostOrder(Graph graph, BlockWalker walker) {
+		HashMap<Block, ? extends List<Block>> blockFollowers = calculateBlockFollowers(graph);
+		incrementBlockVisited(graph);
+		traverseBlocksDepthFirst(graph.getStartBlock(), blockFollowers, walker);
+	}
+
+	private static HashMap<Block, ? extends List<Block>> calculateBlockFollowers(Graph graph) {
+		final HashMap<Block, LinkedList<Block>> blockFollowers = new HashMap<>();
+
+		graph.walkBlocks(new BlockWalker() {
+			@Override
+			public void visitBlock(Block block) {
+				for (Node pred : block.getPreds()) {
+					Block predBlock = (Block) pred.getBlock();
+
+					LinkedList<Block> predsNextBlocks = blockFollowers.get(predBlock);
+					if (predsNextBlocks == null) {
+						predsNextBlocks = new LinkedList<Block>();
+						blockFollowers.put(predBlock, predsNextBlocks);
+					}
+
+					// adding the block at the beginning ensures that the loop body comes after the head
+					// adding the block to the end of the list makes depth first to the end block and then the loop body
+					predsNextBlocks.addFirst(block);
+				}
+			}
+		});
+		return blockFollowers;
+	}
+
+	private static void traverseBlocksDepthFirst(Block block, HashMap<Block, ? extends List<Block>> nextBlocks, BlockWalker walker) {
+		block.markBlockVisited();
+		walker.visitBlock(block);
+
+		List<Block> followers = nextBlocks.get(block);
+
+		if (followers == null)
+			return;
+
+		for (Block followerBlock : followers) {
+			if (!followerBlock.blockVisited())
+				traverseBlocksDepthFirst(followerBlock, nextBlocks, walker);
+		}
+	}
+
+	private static void incrementBlockVisited(Graph graph) {
+		binding_irgraph.inc_irg_block_visited(graph.ptr);
 	}
 
 	private static void allocateRegisters(List<AssemblerOperation> assembler) {
