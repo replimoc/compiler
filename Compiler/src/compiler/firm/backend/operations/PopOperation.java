ae60710cadd74e8762730c6ef9a7b2890161c0d3
Andreas Eberle <email@andreas-eberle.com>
2015-01-12 16:27:04 +0100

    Minor refactoring in AndOperation and PopOperation.

--- Compiler/src/compiler/firm/backend/operations/PopOperation.java
+++ Compiler/src/compiler/firm/backend/operations/PopOperation.java
@@ -2,17 +2,16 @@ package compiler.firm.backend.operations;
 
 import compiler.firm.backend.Bit;
 import compiler.firm.backend.operations.templates.AssemblerBitOperation;
-import compiler.firm.backend.storage.Register;
 import compiler.firm.backend.storage.RegisterBased;
 
 public class PopOperation extends AssemblerBitOperation {
-	private final Register register;
+	private final RegisterBased register;
 
-	public PopOperation(Bit mode, Register register) {
+	public PopOperation(Bit mode, RegisterBased register) {
 		this(null, mode, register);
 	}
 
-	public PopOperation(String comment, Bit mode, Register register) {
+	public PopOperation(String comment, Bit mode, RegisterBased register) {
 		super(comment, mode);
 		this.register = register;
 	}
