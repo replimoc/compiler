230a8c651b6e8f01557323b0ca97dc75a63a97dc
Valentin Zickner <zickner@querformatik.de>
2015-01-18 12:05:07 +0100

    Added workaround for wrong movslq with MemoryPointer as destination (refs #215).

--- Compiler/src/compiler/firm/backend/operations/MovOperation.java
+++ Compiler/src/compiler/firm/backend/operations/MovOperation.java
@@ -4,7 +4,6 @@ import compiler.firm.backend.Bit;
 import compiler.firm.backend.operations.templates.SourceDestinationOperation;
 import compiler.firm.backend.storage.Constant;
 import compiler.firm.backend.storage.Storage;
-import compiler.firm.backend.storage.VirtualRegister;
 
 public class MovOperation extends SourceDestinationOperation {
 
@@ -39,26 +38,27 @@ public class MovOperation extends SourceDestinationOperation {
 
 	}
 
-	@Override
-	public String[] toStringWithSpillcode() {
-		if (hasSpilledRegisters()) {
-			if ((source.getClass() == VirtualRegister.class || source.getClass() == Constant.class)
-					&& (destination.getClass() == VirtualRegister.class || destination.getClass() == Constant.class)) {
-
-				if ((source.isSpilled() && !destination.isSpilled()) || (!source.isSpilled() && destination.isSpilled())) {
-					return new String[] { toString() };
-				} else {
-					Storage oldSource = this.source;
-					this.source = getTemporaryRegister().getRegister(destination.getMode());
-					String[] result = new String[] {
-							new MovOperation(oldSource, this.source).toString(),
-							toString()
-					};
-					this.source = oldSource;
-					return result;
-				}
-			}
-		}
-		return super.toStringWithSpillcode();
-	}
+	// TODO: Fix this for movslq and comment it in
+	// @Override
+	// public String[] toStringWithSpillcode() {
+	// if (hasSpilledRegisters()) {
+	// if ((source.getClass() == VirtualRegister.class || source.getClass() == Constant.class)
+	// && (destination.getClass() == VirtualRegister.class || destination.getClass() == Constant.class)) {
+	//
+	// if ((source.isSpilled() && !destination.isSpilled()) || (!source.isSpilled() && destination.isSpilled())) {
+	// return new String[] { toString() };
+	// } else {
+	// Storage oldSource = this.source;
+	// this.source = getTemporaryRegister().getRegister(destination.getMode());
+	// String[] result = new String[] {
+	// new MovOperation(oldSource, this.source).toString(),
+	// toString()
+	// };
+	// this.source = oldSource;
+	// return result;
+	// }
+	// }
+	// }
+	// return super.toStringWithSpillcode();
+	// }
 }
