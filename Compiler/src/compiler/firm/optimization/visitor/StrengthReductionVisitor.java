302c849d09218af854dcbf6f47376bd08e7417b6
norman465 <abc87545-a@yahoo.de>
2015-01-21 19:41:22 +0100

    Refactored loops and fixed some bugs

--- Compiler/src/compiler/firm/optimization/visitor/StrengthReductionVisitor.java
+++ Compiler/src/compiler/firm/optimization/visitor/StrengthReductionVisitor.java
@@ -3,23 +3,13 @@ package compiler.firm.optimization.visitor;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map.Entry;
+import java.util.Map;
 import java.util.Set;
 
-import firm.BackEdges;
-import firm.BackEdges.Edge;
-import firm.BlockWalker;
-import firm.Graph;
-import firm.Mode;
-import firm.nodes.Anchor;
 import firm.nodes.Block;
 import firm.nodes.Const;
-import firm.nodes.Jmp;
 import firm.nodes.Mul;
 import firm.nodes.Node;
-import firm.nodes.NodeVisitor;
-import firm.nodes.Phi;
-import firm.nodes.Proj;
 import firm.nodes.Start;
 
 public class StrengthReductionVisitor extends OptimizationVisitor<Node> {
@@ -31,9 +21,9 @@ public class StrengthReductionVisitor extends OptimizationVisitor<Node> {
 		}
 	};
 
-	private final HashMap<Node, Node> backedges = new HashMap<>();
-	private final HashMap<Block, Set<Block>> dominators = new HashMap<>();
-	private final HashMap<Node, Node> inductionVariables = new HashMap<>();
+	private HashMap<Node, Node> backedges = new HashMap<>();
+	private HashMap<Block, Set<Block>> dominators = new HashMap<>();
+	private HashMap<Node, Node> inductionVariables = new HashMap<>();
 
 	@Override
 	public HashMap<Node, Node> getLatticeValues() {
@@ -50,15 +40,26 @@ public class StrengthReductionVisitor extends OptimizationVisitor<Node> {
 			}
 		}
 
-		ArrayList<Block> dominatedBlocks = new ArrayList<>();
-		for (Block b : loops) {
+		ArrayList<Block> sameLevelLoops = new ArrayList<>();
+		L1: for (Block b : loops) {
 			if (dominators.containsKey(b) && dominators.get(b).containsAll(loops)) {
-				dominatedBlocks.add(b);
+				for (Map.Entry<Node, Node> entry : backedges.entrySet()) {
+					if (entry.getValue().equals(b)) {
+						if (dominators.containsKey((Block) entry.getKey()) && !dominators.get((Block) entry.getKey()).contains(block)
+								&& !dominatorBlocks.contains(entry.getKey())) {
+							// b and the looṕ header are on the same 'level'
+							sameLevelLoops.add(b);
+							continue L1;
+						}
+					}
+				}
 			}
 		}
-		loops.removeAll(dominatedBlocks);
-		if (loops.size() == 1) {
-			for (Block b : loops) {
+		for (Block b : sameLevelLoops) {
+			loops.remove(b);
+		}
+		for (Block b : loops) {
+			if (dominators.containsKey(b) && dominators.get(b).containsAll(loops)) {
 				return b;
 			}
 		}
@@ -73,7 +74,34 @@ public class StrengthReductionVisitor extends OptimizationVisitor<Node> {
 		if (inductionVariables.containsKey(left)) {
 			if (dominators.get((Block) mul.getBlock()).contains(right.getBlock()) && !mul.getBlock().equals(right.getBlock())) {
 				// right block dominates this mul -> right comes before the loop header
-
+				if (left.getPred(0) != null && isConstant(left.getPred(0))) {
+					Node pred = getInnerMostLoopHeader((Block) mul.getBlock());
+					if (pred == null)
+						return;
+					Node preLoopBlock = pred.getPred(0).getBlock();
+					if (!dominators.get(right.getBlock()).contains(preLoopBlock)) {
+						return;
+					}
+					Node base = mul.getGraph().newMul(preLoopBlock, left.getPred(0), right, mul.getMode());
+					Node incr = inductionVariables.get(left);
+					Node constant = null;
+					if (incr.getPred(0) != null && !incr.getPred(0).equals(left)) {
+						constant = incr.getPred(0);
+					} else if (incr.getPred(1) != null && !incr.getPred(1).equals(left)) {
+						constant = incr.getPred(1);
+					} else {
+						return;
+					}
+					if (!(constant instanceof Const) || !(right instanceof Const)) {
+						return;
+					}
+					Node constNode = mul.getGraph().newConst(((Const) constant).getTarval().mul(((Const) right).getTarval()));
+					Node dummy = mul.getGraph().newDummy(mul.getMode());
+					Node loopPhi = mul.getGraph().newPhi(pred, new Node[] { base, dummy }, mul.getMode());
+					Node add = mul.getGraph().newAdd(mul.getBlock(), loopPhi, constNode, mul.getMode());
+					loopPhi.setPred(1, add);
+					addReplacement(mul, loopPhi);
+				}
 			}
 		} else if (inductionVariables.containsKey(right)) {
 			if (dominators.get((Block) mul.getBlock()).contains(left.getBlock()) && !mul.getBlock().equals(left.getBlock())) {
@@ -99,10 +127,10 @@ public class StrengthReductionVisitor extends OptimizationVisitor<Node> {
 					if (!(constant instanceof Const) || !(left instanceof Const)) {
 						return;
 					}
-					Node c = mul.getGraph().newConst(((Const) constant).getTarval().mul(((Const) left).getTarval()));
+					Node constNode = mul.getGraph().newConst(((Const) constant).getTarval().mul(((Const) left).getTarval()));
 					Node dummy = mul.getGraph().newDummy(mul.getMode());
 					Node loopPhi = mul.getGraph().newPhi(pred, new Node[] { base, dummy }, mul.getMode());
-					Node add = mul.getGraph().newAdd(mul.getBlock(), loopPhi, c, mul.getMode());
+					Node add = mul.getGraph().newAdd(mul.getBlock(), loopPhi, constNode, mul.getMode());
 					loopPhi.setPred(1, add);
 					addReplacement(mul, loopPhi);
 				}
@@ -112,97 +140,9 @@ public class StrengthReductionVisitor extends OptimizationVisitor<Node> {
 
 	@Override
 	public void visit(Start start) {
-		Graph graph = start.getGraph();
-		final Block startBlock = (Block) start.getBlock();
-
-		BlockWalker walker = new BlockWalker() {
-
-			@Override
-			public void visitBlock(Block block) {
-				Set<Block> doms = new HashSet<Block>();
-
-				for (Node node : block.getPreds()) {
-					Block pred = (Block) node.getBlock();
-					Set<Block> dominatedBlocks = dominators.get(pred);
-
-					if (dominatedBlocks == null || dominators.get(pred).contains(block))
-						continue;
-
-					if (doms.size() == 0) {
-						for (Block b : dominatedBlocks) {
-							doms.add(b);
-						}
-					} else {
-						doms.retainAll(dominatedBlocks);
-					}
-				}
-				doms.add(block);
-				doms.add(startBlock);
-
-				if (!dominators.containsKey(block)) {
-					dominators.put(block, doms);
-				} else if (!dominators.get(block).equals(doms)) {
-					dominators.put(block, doms);
-					for (Edge edge : BackEdges.getOuts(block)) {
-						if (edge.node instanceof Anchor)
-							continue;
-						if (((edge.node instanceof Proj && edge.node.getMode().equals(Mode.getX()) || edge.node instanceof Jmp))) {
-							for (Edge backedge : BackEdges.getOuts(edge.node)) {
-								// visit dominated blocks
-								if (dominators.get((Block) backedge.node) != null && dominators.get((Block) backedge.node).contains(block)) {
-									visitBlock((Block) backedge.node);
-								}
-							}
-						}
-					}
-				}
-
-				if (block.getPredCount() > 1) {
-					for (Node node : block.getPreds()) {
-						Set<Block> tmpBlocks = dominators.get(node.getBlock());
-						if (tmpBlocks != null && tmpBlocks.contains(block)) {
-							// found back edge to loop header
-							backedges.put(node.getBlock(), block);
-						}
-					}
-				}
-			}
-		};
-		// ensure that the fixpoint is reached
-		int dominatorCount;
-		int backedgesCount;
-		do {
-			dominatorCount = dominators.values().size();
-			backedgesCount = backedges.values().size();
-			graph.walkBlocks(walker);
-		} while (dominatorCount != dominators.values().size() || backedgesCount != backedges.values().size());
-
-		NodeVisitor visitor = new OptimizationVisitor<Node>() {
-
-			@Override
-			public HashMap<Node, Node> getLatticeValues() {
-				return nodeReplacements;
-			}
-
-			@Override
-			public void visit(Phi phi) {
-				if (!phi.getMode().equals(Mode.getM()) && backedges.containsValue(phi.getBlock())) {
-					Node loopBlock = null;
-					for (Entry<Node, Node> entry : backedges.entrySet()) {
-						if (entry.getValue().equals(phi.getBlock())) {
-							loopBlock = entry.getKey();
-						}
-					}
-					for (Node node : phi.getPreds()) {
-						if (node.getBlock().equals(loopBlock)) {
-							// found operation inside loop
-							inductionVariables.put(phi, node);
-						}
-					}
-				}
-
-			}
-		};
-		graph.walk(visitor);
+		FirmUtils utils = new FirmUtils(start.getGraph());
+		dominators = utils.getDominators();
+		backedges = utils.getBackEdges();
+		inductionVariables = utils.getInductionVariables();
 	}
 }
