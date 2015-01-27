e187e4b943269dba1f8d898e441f94ec3ed90e4d
Andreas Eberle <email@andreas-eberle.com>
2015-01-27 17:36:01 +0100

    Added missing ShrOperation

--- Compiler/src/compiler/firm/backend/operations/ShrOperation.java
+++ Compiler/src/compiler/firm/backend/operations/ShrOperation.java
@@ -1,22 +1,17 @@
 package compiler.firm.backend.operations;
 
-import compiler.firm.backend.Bit;
-import compiler.firm.backend.operations.templates.RegisterConstantOperation;
+import compiler.firm.backend.operations.templates.ConstantRegisterRegisterOperation;
 import compiler.firm.backend.storage.Constant;
 import compiler.firm.backend.storage.RegisterBased;
 
-public class ShrOperation extends RegisterConstantOperation {
+public class ShrOperation extends ConstantRegisterRegisterOperation {
 
-    public ShrOperation(Bit mode, RegisterBased register, Constant constant) {
-        super(mode, register, constant);
-    }
+	public ShrOperation(String comment, Constant constant, RegisterBased source, RegisterBased destination) {
+		super(comment, constant, source, destination);
+	}
 
-    public ShrOperation(String comment, Bit mode, RegisterBased register, Constant constant) {
-        super(comment, mode, register, constant);
-    }
-
-    @Override
-    public String getOperationString() {
-        return String.format("\tshr %s, %s",  getConstant(), getRegister());
-    }
+	@Override
+	public String getOperationString() {
+		return String.format("\tshr %s, %s", getConstant().toString(), destination.toString());
+	}
 }
