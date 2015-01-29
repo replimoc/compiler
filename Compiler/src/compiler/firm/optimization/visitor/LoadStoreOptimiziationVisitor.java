6bfe5c01e52fd47d7f367d4bf62b89737d7431bc
Aleksej Frank <aleksejfrank@googlemail.com>
2015-01-30 00:04:26 +0100

    implemented basic Store-Store-Optimization

--- Compiler/src/compiler/firm/optimization/visitor/LoadStoreOptimiziationVisitor.java
+++ Compiler/src/compiler/firm/optimization/visitor/LoadStoreOptimiziationVisitor.java
@@ -8,6 +8,7 @@ import firm.Mode;
 import firm.nodes.Load;
 import firm.nodes.Node;
 import firm.nodes.Proj;
+import firm.nodes.Store;
 
 public class LoadStoreOptimiziationVisitor extends OptimizationVisitor<Node> {
 
@@ -19,29 +20,49 @@ public class LoadStoreOptimiziationVisitor extends OptimizationVisitor<Node> {
 	};
 
 	private Load lastLodeNode;
+	private Store lastStoreNode;
 
-	private boolean existSavePathToLastLoad(Load node) {
-		if (lastLodeNode != null && // if last node != null
-				node.getBlock().equals(lastLodeNode.getBlock()) && // both loads in the same block
+	private boolean existSavePathToLastMemNode(Node node, Node memoryNode) {
+		if (memoryNode != null && // if last node != null
+				node.getBlock().equals(memoryNode.getBlock()) && // both loads in the same block
 				node.getPred(0) instanceof Proj) {
 			Proj proj = (Proj) node.getPred(0);
-			if (proj.getPred() instanceof Load && proj.getPred().equals(lastLodeNode) &&
-					node.getPred(1).equals(lastLodeNode.getPred(1))) {
+			if ((proj.getPred() instanceof Load || proj.getPred() instanceof Store) && proj.getPred().equals(memoryNode)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
+	private boolean dependOnSameNode(Node node1, Node node2) {
+		// check if the two memory nodes are dependent on equal nodes
+
+		// if unequal number of nodes, then quit
+		if (node1.getPredCount() != node2.getPredCount()) {
+			return false;
+		}
+
+		// first node is memory
+
+		// second successor is memory adress
+		if (node1.getPred(1).equals(node2.getPred(1)) == false) {
+			return false;
+		}
+
+		// third is value
+
+		return true;
+	}
+
 	private String getMethodName(Node node) {
 		return node.getGraph().getEntity().getLdName();
 	}
 
-	private void uniteProjSuccessors(Load load) {
+	private void uniteProjSuccessors(Node node) {
 		Proj memProj = null;
 		Proj nonMemProj = null;
 
-		for (Edge edge : BackEdges.getOuts(load)) {
+		for (Edge edge : BackEdges.getOuts(node)) {
 			if (edge.node.getMode().equals(Mode.getM())) {
 				if (memProj == null) {
 					memProj = (Proj) edge.node;
@@ -63,7 +84,7 @@ public class LoadStoreOptimiziationVisitor extends OptimizationVisitor<Node> {
 			}
 		}
 
-		System.out.println(load + " united (" + BackEdges.getNOuts(load) + ")");
+		// System.out.println(node + " united (" + BackEdges.getNOuts(node) + ")");
 	}
 
 	@Override
@@ -73,8 +94,7 @@ public class LoadStoreOptimiziationVisitor extends OptimizationVisitor<Node> {
 
 	@Override
 	public void visit(Load load) {
-		if (existSavePathToLastLoad(load)) {
-			// System.out.println(load + " is being optimized!");
+		if (existSavePathToLastMemNode(load, lastLodeNode) && dependOnSameNode(load, lastLodeNode)) {
 			// System.out.println(getMethodName(load) + ":" + load);
 
 			for (Edge edge : BackEdges.getOuts(load)) {
@@ -87,5 +107,23 @@ public class LoadStoreOptimiziationVisitor extends OptimizationVisitor<Node> {
 		}
 
 		lastLodeNode = load;
+	}
+
+	@Override
+	public void visit(Store store) {
+		if (existSavePathToLastMemNode(store, lastStoreNode)) {
+			// System.out.println("safe path");
+			if (dependOnSameNode(store, lastStoreNode)) {
+				System.out.println(getMethodName(store) + ":" + store);
+
+				store.setPred(0, lastStoreNode.getPred(0));
+
+				// uniteProjSuccessors(lastStoreNode);
+
+				addReplacement(lastStoreNode, lastStoreNode.getGraph().newBad(lastStoreNode.getMode()));
+			}
+		}
+
+		lastStoreNode = store;
 	}
 }
