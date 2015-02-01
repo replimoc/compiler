f52719cca8061f46d64cead3e3a08daceaa0532b
norman465 <abc87545-a@yahoo.de>
2015-02-01 10:39:08 +0100

    Added more tests

---
+++ Compiler/testdata/optimization/loops/LoopOverflows.java
@@ -0,0 +1,31 @@
+class Test {
+	public static void main(String[] args) {
+		int i = 17;	
+		while(i > 11) {	
+			i = i + 7;
+		}
+		i = i - 1;
+		System.out.println(i);
+		
+		int j = -19;	
+		while(j < 9) {
+			j = j - 1;
+		}
+		j = j + 1;
+		System.out.println(j);
+		
+		int k = 4;
+		while(k <= 9) {
+			k = k - 12;
+		}
+		k = k + 1;
+		System.out.println(k);
+		
+		int a = 6;
+		while(a > 4) {
+			a = a + 11;
+		}
+		a = a - 1;
+		System.out.println(a);
+	}
+}
---
+++ Compiler/testdata/optimization/loops/LoopUnroll6.java
@@ -0,0 +1,29 @@
+class Test {
+	public static void main(String[] args) {
+		int i = 29;	
+		while(i >= 0) {	
+			i = i + 3;
+		}
+		i = i - 1;
+		System.out.println(i);
+		
+		int j = -16;	
+		while(j <= 0) {
+			j = j - 4;
+		}
+		j = j + 1;
+		System.out.println(j);
+		
+		int k = 4;
+		while(k <= 9) {
+			System.out.println(k);
+			k = k + 1;
+		}
+		
+		int a = 6;
+		while(a > 4) {
+			a = a - 1;
+			System.out.println(a);
+		}
+	}
+}
