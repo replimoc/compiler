42d23685a7ef4f69cafa775e4f3d3b78fdac280f
norman465 <abc87545-a@yahoo.de>
2015-02-03 18:15:54 +0100

    Added and fixed loop unroll tests

---
+++ Compiler/testdata/optimization/loops/LoopReplace.java
@@ -0,0 +1,32 @@
+class Test {
+	public static void main(String[] args) {
+		int i = 12;	
+		while(i >= -15) {	
+			i = i + 7;
+		}
+		i = i + 1;
+		System.out.println(i);
+		
+		int j = -64;	
+		while(j <= -10) {
+			j = j - 9;
+		}
+		j = j + 4;
+		System.out.println(j);
+
+		int k = 55;	
+		while(k >= 0) {	
+			k = k + 5;
+		}
+		k = k - 1;
+		System.out.println(k);
+		
+		int x = 3;	
+		while(x <= 5) {
+			x = x - 2;
+		}
+		x = x + 11;
+		System.out.println(x);
+		
+	}
+}
