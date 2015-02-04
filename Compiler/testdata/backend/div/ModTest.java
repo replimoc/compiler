357b8de4328bfc814118c1f179b723ce4167a790
Andreas Eberle <email@andreas-eberle.com>
2015-02-04 23:47:21 +0100

    Moved ModTest.java to backend/div folder

--- Compiler/testdata/backend/div/ModTest.java
+++ Compiler/testdata/backend/div/ModTest.java
@@ -3,8 +3,8 @@ class Test {
 		System.out.println(2342134 % 33);
 		System.out.println(-2346231 % 234);
 
-		int a = -124;
-		int b = 3242340;
+		int a = -124 + 0;
+		int b = 3242340 + 1;
 
 		System.out.println(b % a);
 	}
